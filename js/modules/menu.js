3e4aebe258b26f571f654efd0dbe8c8b945e86e9
Igor <matosigor00@gmail.com>
2022-10-17 16:27:02 -0300

    relative-path-fix

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -2,12 +2,11 @@ export default function menuMobile() {
   function menuEvents() {
     const menu = document.querySelector('.menu-mobile');
     const navigation = document.querySelector('.menu-links');
-    // const navLinks = document.querySelectorAll('.menu-link');
 
     function openMenu(e) {
-      // if (e.type === 'touchstart') {
-      //   e.preventDefault();
-      // }
+      if (e.type === 'touchstart') {
+        e.preventDefault();
+      }
       menu.classList.toggle('open');
       navigation.classList.toggle('open');
       if (menu.classList.contains('open')) {
@@ -15,16 +14,10 @@ export default function menuMobile() {
       } else {
         menu.ariaExpanded = false;
       }
-
-      // ['click', 'touchstart'].forEach((event) => {
-      //   navLinks.forEach((link) => {
-      //     link.addEventListener(event, openMenu);
-      //   });
-      // });
     }
 
     menu.addEventListener('click', openMenu);
-    // menu.addEventListener('touchstart', openMenu);
+    menu.addEventListener('touchstart', openMenu);
   }
 
   menuEvents();
